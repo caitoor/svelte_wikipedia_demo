02a5e628c89da330c180e5c90b11a30f623aa5b9
Fabian <mail@rauch-fabian.de>
2025-01-21 22:29:28 +0100

    added image

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,27 @@
 <script>
   let animalName = "";
   let excerpt = "";
+  let imageUrl = "";
 
   async function fetchExcerpt(animal) {
     const response = await fetch(
       `https://en.wikipedia.org/api/rest_v1/page/summary/${animal}`,
     );
     const data = await response.json();
-    return data.extract;
+    return { extract: data.extract, image: data.thumbnail ? data.thumbnail.source : '' };
   }
 </script>
 
 <input type="text" bind:value={animalName} placeholder="Enter animal name" />
 <button
   on:click={async () => {
-    excerpt = await fetchExcerpt(animalName);
+    const result = await fetchExcerpt(animalName);
+    excerpt = result.extract;
+    imageUrl = result.image;
   }}>Submit</button
 >
 
 <p>{excerpt}</p>
+{#if imageUrl}
+  <img src={imageUrl} alt={animalName} />
+{/if}
