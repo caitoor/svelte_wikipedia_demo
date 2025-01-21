54dd79d75c12e4812d683d005f1a47942f649495
Fabian <mail@rauch-fabian.de>
2025-01-21 22:28:11 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  let animalName = "";
+  let excerpt = "";
+
+  async function fetchExcerpt(animal) {
+    const response = await fetch(
+      `https://en.wikipedia.org/api/rest_v1/page/summary/${animal}`,
+    );
+    const data = await response.json();
+    return data.extract;
+  }
+</script>
+
+<input type="text" bind:value={animalName} placeholder="Enter animal name" />
+<button
+  on:click={async () => {
+    excerpt = await fetchExcerpt(animalName);
+  }}>Submit</button
+>
+
+<p>{excerpt}</p>
